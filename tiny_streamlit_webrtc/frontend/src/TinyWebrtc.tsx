02b48614d54239e2695d87c33d9f48240b077470
Yuichiro Tsuchiya <t.yic.yt@gmail.com>
2021-01-24 03:59:10 +0900

    Remove comments and docstrings

--- tiny_streamlit_webrtc/frontend/src/TinyWebrtc.tsx
+++ tiny_streamlit_webrtc/frontend/src/TinyWebrtc.tsx
@@ -9,22 +9,12 @@ interface State {
   numClicks: number
 }
 
-/**
- * This is a React-based component template. The `render()` function is called
- * automatically when your component should be re-rendered.
- */
 class TinyWebrtc extends StreamlitComponentBase<State> {
   public state = { numClicks: 0 }
 
   public render = (): ReactNode => {
-    // Arguments that are passed to the plugin in Python are accessible
-    // via `this.props.args`. Here, we access the "name" arg.
     const name = this.props.args["name"]
 
-    // Show a button and some text.
-    // When the button is clicked, we'll increment our "numClicks" state
-    // variable, and send its new value back to Streamlit, where it'll
-    // be available to the Python program.
     return (
       <span>
         Hello, {name}! &nbsp;
@@ -35,10 +25,7 @@ class TinyWebrtc extends StreamlitComponentBase<State> {
     )
   }
 
-  /** Click handler for our "Click Me!" button. */
   private onClicked = (): void => {
-    // Increment state.numClicks, and pass the new value back to
-    // Streamlit via `Streamlit.setComponentValue`.
     this.setState(
       prevState => ({ numClicks: prevState.numClicks + 1 }),
       () => Streamlit.setComponentValue(this.state.numClicks)
@@ -46,9 +33,4 @@ class TinyWebrtc extends StreamlitComponentBase<State> {
   }
 }
 
-// "withStreamlitConnection" is a wrapper function. It bootstraps the
-// connection between your component and the Streamlit app, and handles
-// passing arguments from Python -> Component.
-//
-// You don't need to edit withStreamlitConnection (but you're welcome to!).
 export default withStreamlitConnection(TinyWebrtc)
