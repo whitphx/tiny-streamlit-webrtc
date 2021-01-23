f6daf280c650c04ae45f114fff4a4d0fd39a14c1
Yuichiro Tsuchiya <t.yic.yt@gmail.com>
2021-01-24 04:34:07 +0900

    Remove all implementations

--- tiny_streamlit_webrtc/__init__.py
+++ tiny_streamlit_webrtc/__init__.py
@@ -14,22 +14,12 @@ else:
     _component_func = components.declare_component("tiny_streamlit_webrtc", path=build_dir)
 
 
-def tiny_streamlit_webrtc(name, key=None):
-    component_value = _component_func(name=name, key=key, default=0)
+def tiny_streamlit_webrtc(key=None):
+    component_value = _component_func(key=key, default=0)
     return component_value
 
 
 if not _RELEASE:
     import streamlit as st
 
-    st.subheader("Component with constant args")
-
-    num_clicks = tiny_streamlit_webrtc("World")
-    st.markdown("You've clicked %s times!" % int(num_clicks))
-
-    st.markdown("---")
-    st.subheader("Component with variable args")
-
-    name_input = st.text_input("Enter a name", value="Streamlit")
-    num_clicks = tiny_streamlit_webrtc(name_input, key="foo")
-    st.markdown("You've clicked %s times!" % int(num_clicks))
+    tiny_streamlit_webrtc()
--- tiny_streamlit_webrtc/frontend/src/TinyWebrtc.tsx
+++ tiny_streamlit_webrtc/frontend/src/TinyWebrtc.tsx
@@ -1,35 +1,17 @@
 import {
   Streamlit,
   StreamlitComponentBase,
-  withStreamlitConnection
-} from "streamlit-component-lib";
+  withStreamlitConnection,
+} from "streamlit-component-lib"
 import React, { ReactNode } from "react"
 
-interface State {
-  numClicks: number
-}
+interface State {}
 
 class TinyWebrtc extends StreamlitComponentBase<State> {
-  public state = { numClicks: 0 }
+  public state = {}
 
   public render = (): ReactNode => {
-    const name = this.props.args["name"]
-
-    return (
-      <span>
-        Hello, {name}! &nbsp;
-        <button onClick={this.onClicked} disabled={this.props.disabled}>
-          Click Me!
-        </button>
-      </span>
-    )
-  }
-
-  private onClicked = (): void => {
-    this.setState(
-      prevState => ({ numClicks: prevState.numClicks + 1 }),
-      () => Streamlit.setComponentValue(this.state.numClicks)
-    )
+    return <span></span>
   }
 }
 
